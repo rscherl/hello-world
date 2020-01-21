1b3ad265de80aae5979975359e5e999b6edb4ecb
rscherl <rscherl@mu60308.home>
2020-01-20 19:26:17 -0500

    load files to git

---
+++ cs176-hw00/src/edu/monmouth/cs176/s1234567/lab01/Tester.java
@@ -0,0 +1,12 @@
+package edu.monmouth.cs176.s1234567.lab01;
+
+public class Tester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Worker w = new Worker("Hello World");
+		w.sendMssg();
+
+	}
+
+}
---
+++ cs176-hw00/src/edu/monmouth/cs176/s1234567/lab01/Worker.java
@@ -0,0 +1,13 @@
+package edu.monmouth.cs176.s1234567.lab01;
+
+public class Worker {
+	String mssg;
+	public Worker(String inMssg) {
+		mssg = inMssg;
+	}
+	public void sendMssg() {
+		System.out.println(mssg);
+	}
+			
+
+}
